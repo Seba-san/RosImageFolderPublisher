d489172798b60cc3da04ccc70db344cb65eddae9
seba <sansoni.sebas@gmail.com>
2022-02-17 16:00:30 -0300

    support for stereo camera was added

--- src/image_folder_publisher/scripts/image_folder_publisher.py
+++ src/image_folder_publisher/scripts/image_folder_publisher.py
@@ -19,14 +19,18 @@ class image_folder_publisher:
     def __init__(self):
         self.__app_name = "image_folder_publisher"
 
-        self._cv_bridge = CvBridge()
+        self._cv_bridge_left = CvBridge()
+        self._cv_bridge_right = CvBridge()
 
-        self._topic_name = rospy.get_param('~topic_name', '/image_raw')
-        rospy.loginfo("[%s] (topic_name) Publishing Images to topic  %s", self.__app_name, self._topic_name)
+        self._topic_name_left = rospy.get_param('~topic_name_left', '/image_raw_left')
+        self._topic_name_right = rospy.get_param('~topic_name_right', '/image_raw_right')
+        rospy.loginfo("[%s] (topic_name) Publishing Images to topic  %s", self.__app_name, self._topic_name_left)
+        rospy.loginfo("[%s] (topic_name) Publishing Images to topic  %s", self.__app_name, self._topic_name_right)
 
-        self._image_publisher = rospy.Publisher(self._topic_name, Image, queue_size=1)
+        self._image_publisher_left = rospy.Publisher(self._topic_name_left, Image, queue_size=1)
+        self._image_publisher_right = rospy.Publisher(self._topic_name_right, Image, queue_size=1)
 
-        self._rate = rospy.get_param('~publish_rate', 1)
+        self._rate = rospy.get_param('~publish_rate', 15)
         rospy.loginfo("[%s] (publish_rate) Publish rate set to %s hz", self.__app_name, self._rate)
 
         self._sort_files = rospy.get_param('~sort_files', True)
@@ -40,6 +44,7 @@ class image_folder_publisher:
 
         self._image_folder = rospy.get_param('~image_folder', '')
         if self._image_folder == '' or not os.path.exists(self._image_folder) or not os.path.isdir(self._image_folder):
+            #import pdb; pdb.set_trace()
             rospy.logfatal("[%s] (image_folder) Invalid Image folder", self.__app_name)
             sys.exit(0)
         rospy.loginfo("[%s] Reading images from %s", self.__app_name, self._image_folder)
@@ -47,20 +52,33 @@ class image_folder_publisher:
     def run(self):
         ros_rate = rospy.Rate(self._rate)
 
-        files_in_dir = [f for f in listdir(self._image_folder) if isfile(join(self._image_folder, f))]
+        dir_left=self._image_folder + '/left/'
+        dir_right=self._image_folder + '/right/'
+        #import pdb; pdb.set_trace()
+
+        files_in_dir_right = [f for f in listdir(dir_right) if  isfile(join(dir_right, f))]
+        files_in_dir_left = [f for f in listdir(dir_left) if isfile(join(dir_left, f))]
         if self._sort_files:
-            files_in_dir.sort()
+            files_in_dir_left.sort()
         try:
             while self._loop != 0:
-                for f in files_in_dir:
+                for f in files_in_dir_left:
                     if not rospy.is_shutdown():
-                        if isfile(join(self._image_folder, f)):
-                            cv_image = cv2.imread(join(self._image_folder, f))
-                            if cv_image is not None:
-                                ros_msg = self._cv_bridge.cv2_to_imgmsg(cv_image, "bgr8")
+                        if isfile(join(dir_left, f)):
+                            cv_image_left = cv2.imread(join(dir_left, f))
+                            cv_image_right = cv2.imread(join(dir_right,'right'+f[4:]))
+
+                            #import pdb; pdb.set_trace()
+                            if cv_image_left is not None:
+                                ros_msg = self._cv_bridge_left.cv2_to_imgmsg(cv_image_left, "bgr8")
                                 ros_msg.header.frame_id = self._frame_id
                                 ros_msg.header.stamp = rospy.Time.now()
-                                self._image_publisher.publish(ros_msg)
+                                self._image_publisher_left.publish(ros_msg)
+                                ros_msg = self._cv_bridge_right.cv2_to_imgmsg(cv_image_right, "bgr8")
+                                ros_msg.header.frame_id = self._frame_id
+                                ros_msg.header.stamp = rospy.Time.now()
+                                self._image_publisher_right.publish(ros_msg)
+
                                 rospy.loginfo("[%s] Published %s", self.__app_name, join(self._image_folder, f))
                             else:
                                 rospy.loginfo("[%s] Invalid image file %s", self.__app_name, join(self._image_folder, f))
@@ -70,7 +88,6 @@ class image_folder_publisher:
                 self._loop = self._loop - 1
         except CvBridgeError as e:
             rospy.logerr(e)
-
 
 def main(args):
     rospy.init_node('image_folder_publisher', anonymous=True)
